5b1d53d4671d81f32b339d5bd0389fdafe467820
ianchan3 <ianchan3138@gmail.com>
2022-08-05 14:09:55 -0700

    First Commit

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', {
+    'dogs': dogs
+  })
