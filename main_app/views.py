4a8e96d0ff4f015c87a43803ebadccce4bf15164
ianchan3 <ianchan3138@gmail.com>
2022-08-05 14:24:48 -0700

    Part 1 Complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'foul little demon', 3),
+  Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Dog('Raven', 'black tripod', '3 legged cat', 4)
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
