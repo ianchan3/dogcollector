6f6fb229001c249a686df8119fb9ed2ded3d9a4d
ianchan3 <ianchan3138@gmail.com>
2022-08-10 11:24:51 -0700

    Add Feeding and Date

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
-# Define the home view
+# Create your views here.
 def home(request):
   return render(request, 'home.html')
 
@@ -17,18 +18,41 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', {'dog': dog})
+  feeding_form = FeedingForm()
+  return render(
+    request,
+    'dogs/detail.html',
+    {'dog': dog, 'feeding_form': feeding_form}
+  )
 
 class DogCreate(CreateView):
   model = Dog
   fields = '__all__'
+  # success_url = '/dogs/'
+
+  # def get_success_url(self):
+  #   return f'/dogs/{self.object.id}'
 
 class DogUpdate(UpdateView):
   model = Dog
-  # Let's disallow the renaming of a cat by
+  # Let's disallow the renaming of a dog by
   # excluding the name field
   fields = ['breed', 'description', 'age']
 
 class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs/'
+
+def add_feeding(request, dog_id):
+  # create a FeedingForm instance using
+  # the data that was submitted via the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # can't save the form/object to the db
+    # until we've assigned a dog_id
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
